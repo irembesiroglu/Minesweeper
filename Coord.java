2706943366ee258661553227a48d43a77ead7c6b
irembesiroglu <144672445+irembesiroglu@users.noreply.github.com>
2024-02-02 22:41:36 +0300

    Create Coord.java

---
+++ Coord.java
@@ -0,0 +1,20 @@
+package minesweeper;
+
+public class Coord {
+    public int x;
+    public int y;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Coord)) {
+            return super.equals(o);
+        }
+        Coord to = (Coord) o;
+        return to.x == x && to.y == y;
+    }
+}
